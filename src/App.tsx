7cd44354a6f5ae90dd26e39cc994ab4b148865a5
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-03-01 19:46:17 +0300

    sprint 01 week 03: add createTask, add id for task

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import './App.css'
 import {TaskType, TodolistItem} from "./components/todolistItem.tsx";
 import {useState} from "react";
+import {v1} from 'uuid';
 
 export type FilterValuesTypes = "all" | "active" | "completed";
 
 export const App = () => {
 
   const tasks1: TaskType[] = [
-    {id: 1, title: 'HTML&CSS', isDone: true},
-    {id: 2, title: 'JS', isDone: true},
-    {id: 3, title: 'ReactJS', isDone: false},
-    {id: 4, title: 'Redux', isDone: false},
-    {id: 5, title: 'Typescript', isDone: false},
-    {id: 6, title: 'RTK query', isDone: false},
+    {id: v1(), title: 'HTML&CSS', isDone: true},
+    {id: v1(), title: 'JS', isDone: true},
+    {id: v1(), title: 'ReactJS', isDone: false},
+    {id: v1(), title: 'Redux', isDone: false},
+    {id: v1(), title: 'Typescript', isDone: false},
+    {id: v1(), title: 'RTK query', isDone: false},
   ]
 
   const [tasks, setTasks] = useState<TaskType[]>(tasks1);
 
-  const removeTask = (id: number) => {
+  const removeTask = (id: string) => {
     const newTasks: TaskType[] = [];
     for (let i = 0; i < tasks.length; i++) {
       if (tasks[i].id !== id) {
@@ -27,12 +28,23 @@ export const App = () => {
     setTasks(newTasks);
   }
 
+  const createTask = (title: string) => {
+    const newTask = {
+      id: v1(),
+      title: title,
+      isDone: false
+    }
+    const newTasks = [newTask, ...tasks]
+    setTasks(newTasks)
+  }
+
   return (
     <div className="app">
       <TodolistItem title={"What to learn"}
                     tasks={tasks}
                     date={"2025-02-28"}
                     removeTask={removeTask}
+                    createTask={createTask}
       />
     </div>
   )
--- src/components/todolistItem.tsx
+++ src/components/todolistItem.tsx
@@ -1,24 +1,26 @@
 import {Button} from "./button.tsx";
 import {FilterValuesTypes} from "../App.tsx";
-import {useState} from "react";
+import {ChangeEvent, useState} from "react";
 
 export type TodolistItemProps = {
   title: string
   tasks: TaskType[]
   date?: string
-  removeTask: (tasksId: number) => void
+  removeTask: (tasksId: string) => void
+  createTask: (title: string) => void
 }
 
 export type TaskType = {
-  id: number,
+  id: string,
   title: string,
   isDone: boolean
 }
 
 export const TodolistItem = (
-  {title, tasks, date, removeTask}: TodolistItemProps) => {
+  {title, tasks, date, removeTask, createTask}: TodolistItemProps) => {
 
   const [filter, setFilter] = useState<FilterValuesTypes>('all');
+  const [taskTitle, setTaskTitle] = useState<string>('');
 
   const getTasksForTodolist = (allTasks: TaskType[], newFilter: FilterValuesTypes) => {
     switch (newFilter) {
@@ -33,26 +35,50 @@ export const TodolistItem = (
 
   const tasksForTodolist: TaskType[] = getTasksForTodolist(tasks, filter);
 
-  const onClickHandlerDeleteTask = (taskId: number) => {
+  const onClickHandlerDeleteTask = (taskId: string) => {
     removeTask(taskId);
+  }
+
+  const changeTaskTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setTaskTitle(event.currentTarget.value)
+  }
+
+  // @ts-ignore
+  const createTaskOnEnterHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      createTaskHandler()
+    }
+  }
+
+  const createTaskHandler = () => {
+    createTask(taskTitle)
+    setTaskTitle('')
   }
 
   return (
     <div>
       <h3>{title}</h3>
       <div>
-        <input/>
-        <Button title={"+"}/>
+        <input value={taskTitle}
+               onChange={changeTaskTitleHandler}
+               onKeyDown={createTaskOnEnterHandler}
+        />
+        <Button title={"+"} onClick={createTaskHandler}/>
       </div>
       {tasks.length === 0
         ? (<p>No tasks</p>)
         : (<ul>
           {tasksForTodolist.map((task: TaskType) => {
+
+            const deleteTaskHandler = () => {
+              onClickHandlerDeleteTask(task.id)
+            }
+
             return (
               <li key={task.id}>
                 <input type={"checkbox"} checked={task.isDone}/>
                 <span>{task.title}</span>
-                <Button title={"x"} onClick={() => onClickHandlerDeleteTask(task.id)}/>
+                <Button title={"x"} onClick={deleteTaskHandler}/>
               </li>
             )
           })}
