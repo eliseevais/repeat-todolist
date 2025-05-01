be92d47f8478a5272fc54058a94924531167e0b7
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-05-01 13:45:39 +0300

    sprint 03 week 04: new files from archive and prettier changes

--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -1,23 +1,21 @@
-import { instance } from "@/common/instance/instance.ts";
-import { Todolist } from "@/features/todolists/api/todolistsApi.types.ts";
+import { instance } from "@/common/instance";
 import type { BaseResponse } from "@/common/types";
+import type { Todolist } from "./todolistsApi.types";
 
 export const todolistsApi = {
   getTodolists() {
-    return instance.get<Todolist[]>(`/todo-lists`);
+    return instance.get<Todolist[]>("/todo-lists");
   },
   changeTodolistTitle(payload: { id: string; title: string }) {
-    const { title, id } = payload;
+    const { id, title } = payload;
     return instance.put<BaseResponse>(`/todo-lists/${id}`, { title });
   },
-  createTodolist(payload: { title: string }) {
-    const { title } = payload;
-    return instance.post<BaseResponse<{ item: Todolist }>>(`/todo-lists`, {
+  createTodolist(title: string) {
+    return instance.post<BaseResponse<{ item: Todolist }>>("/todo-lists", {
       title,
     });
   },
-  deleteTodolist(payload: { id: string }) {
-    const { id } = payload;
+  deleteTodolist(id: string) {
     return instance.delete<BaseResponse>(`/todo-lists/${id}`);
   },
 };
